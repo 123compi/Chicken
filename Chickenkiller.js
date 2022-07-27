2daf50c9a4178711a3acf31c818029ac34200d4e
123compi <109691574+123compi@users.noreply.github.com>
2022-07-27 10:22:01 -0700

    Create Chickenkiller.js

---
+++ Chickenkiller.js
@@ -0,0 +1,8 @@
+function ReadDropMenu(){
+  var chicken_killer=  document.getElementById('chicken');
+  alert(chicken_killer.value)
+}
+
+
+document.getElementById('chicken').addEventListener('change',ReadDropmenu());
+
